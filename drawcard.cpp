d4a903c7985b2084596f153d1922f16b4f549dc6
Kace Butkiewicz <kcbutkie@umich.edu>
2025-04-13 18:32:44 -0400

    Initial Attempt

---
+++ drawcard.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+string drawCard(string deck[4][13]) {
+    int i=0, j=0;
+    string drawnCard = "0";
+    srand(time(0));
+    while (drawnCard == "0") {
+        i = rand() % 4;
+        j = rand() % 13;
+        drawnCard = deck[i][j];
+    }
+    deck[i][j] = "0";
+    return drawnCard;
+}
+
